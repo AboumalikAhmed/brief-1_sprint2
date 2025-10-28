a78a962f141232f001a642cd6d9804bff9c97145
AboumalikAhmed <aboumalikahmed25@gmail.com>
2025-10-28 17:04:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let clock = document.querySelector(".clock");
+let clock_hour = clock.querySelector(".hour");
+let clock_minutes = clock.querySelector(".minute");
+
+let clock_secondes = clock.querySelector(".second");
